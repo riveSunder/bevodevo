4eccff14f72d25c89de9f0787edaed2c1d2ff49b
RiveSunder <rivesunder@protonmail.com>
2022-09-07 17:13:51 -0600

    add more tests for mlps set_params

--- bevodevo/tests/policies/test_mlps.py
+++ bevodevo/tests/policies/test_mlps.py
@@ -4,7 +4,13 @@ import unittest
 
 import numpy as np
 
-from bevodevo.policies.mlps import MLPPolicy, HebbianMLP,  ABCHebbianMLP
+from bevodevo.policies.mlps import MLPPolicy, \
+        HebbianMLP,\
+        ABCHebbianMLP,\
+        HebbianCAMLP, \
+        HebbianCAMLP2, \
+        CPPNHebbianMLP, \
+        CPPNMLPPolicy
 
 class TestMLPPolicy(unittest.TestCase):
 
@@ -56,6 +62,76 @@ class TestABCHebbianMLP(TestMLPPolicy):
         output = self.policy(x)
 
         self.assertEqual(output.shape[-1], self.policy.action_dim)
+
+class TestHebbianCAMLP(TestMLPPolicy):
+
+    def setUp(self):
+        self.policy = HebbianCAMLP(params=None)
+    def test_mlp_forward(self):
+        pass
+
+class TestHebbianCAMLP2(TestMLPPolicy):
+
+    def setUp(self):
+        self.policy = HebbianCAMLP2(params=None)
+
+    def test_mlp_forward(self):
+        pass
+
+
+class TestCPPNHebbianMLP(TestMLPPolicy):
+
+    def setUp(self):
+        self.policy = CPPNHebbianMLP(params=None)
+
+    def test_mlp_forward(self):
+        pass
+
+    def test_set_params(self):
+        
+        my_params = self.policy.get_cppn_params()
+
+        new_params = np.random.randn(*my_params.shape)
+
+        self.policy.set_params(new_params)
+        recovered_params = self.policy.get_cppn_params()
+
+        # some precision is lost going back and forth
+        # between numpy and torch here, therefore 1e-6
+        
+        self.assertGreater(1e-6, np.abs(new_params - recovered_params).max())
+
+        self.policy.set_params(my_params)
+        recovered_params = self.policy.get_cppn_params()
+
+        self.assertNotIn(False, my_params == recovered_params)
+
+class TestCPPNMLPPolicy(TestMLPPolicy):
+
+    def setUp(self):
+        self.policy = CPPNMLPPolicy(params=None)
+
+    def test_mlp_forward(self):
+        pass
+
+    def test_set_params(self):
+        
+        my_params = self.policy.get_cppn_params()
+
+        new_params = np.random.randn(*my_params.shape)
+
+        self.policy.set_params(new_params)
+        recovered_params = self.policy.get_cppn_params()
+
+        # some precision is lost going back and forth
+        # between numpy and torch here, therefore 1e-6
+        
+        self.assertGreater(1e-6, np.abs(new_params - recovered_params).max())
+
+        self.policy.set_params(my_params)
+        recovered_params = self.policy.get_cppn_params()
+
+        self.assertNotIn(False, my_params == recovered_params)
 
 if __name__ == "__main__": #pragma: no cover
     unittest.main(verbosity=2)
--- bevodevo/tests/test_all.py
+++ bevodevo/tests/test_all.py
@@ -2,6 +2,10 @@ import unittest
 
 from bevodevo.tests.policies.test_mlps import TestMLPPolicy,\
     TestHebbianMLP,\
+    TestHebbianCAMLP,\
+    TestHebbianCAMLP2,\
+    TestCPPNHebbianMLP,\
+    TestCPPNMLPPolicy,\
     TestABCHebbianMLP
 
 if __name__ == "__main__": #pragma: no cover
