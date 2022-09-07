81367dee84b0a8738aa36774c2793f0000ddbfe1
RiveSunder <rivesunder@protonmail.com>
2022-09-07 16:45:23 -0600

    update numpy dependency in setup.py (fix pybullet incompatibility with older np)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(\
         name="bevodevo",\
         py_modules=["bevodevo"],\
-        install_requires=["numpy==1.18.4",\
+        install_requires=["numpy>=1.18.4",\
                         "torch==1.5.1",\
                         "mpi4py==3.0.3",\
                         "gym[atari,box2d,classic_control]~=0.15.3",\
